eefdf838464ddf1e87e6f653d48faa43d83c1ed0
ddk9499 <ddk9499@gmail.com>
2019-03-12 21:32:33 +0500

    0.0.2

--- app/src/main/java/uz/dkamaloff/quizapp/MainActivity.kt
+++ app/src/main/java/uz/dkamaloff/quizapp/MainActivity.kt
@@ -7,16 +7,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+	lateinit var question: Question
+	var currentIndex = 0
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		val question: Question = Question("Quyosh G\'arbdan chiqadi", false)
 		val questions = mutableListOf<Question>()
+		questions.add(Question("Quyosh G\'arbdan chiqadi", false))
 		questions.add(Question("Samarkand is the capital of Uzbekistan", false))
 		questions.add(Question("Nil is the longest river", true))
 		questions.add(Question("Current year is 2019", true))
 		questions.add(Question("Moon is the planet", false))
+
+		question = questions[currentIndex]
 
 		question_tv.text = question.questionText
 
@@ -26,6 +31,16 @@ class MainActivity : AppCompatActivity() {
 
 		false_btn.setOnClickListener {
 			checkAnswer(false, question.answer)
+		}
+
+		next_btn.setOnClickListener {
+			if (currentIndex == questions.size - 1) {
+				currentIndex = 0
+			} else {
+				currentIndex = currentIndex % (questions.size - 1) + 1
+			}
+			question = questions[currentIndex]
+			question_tv.text = question.questionText
 		}
 	}
 
